81d7200e81596fd3ddf8b5914ab2a7df6c682a05
wallanmota <wallanmota.13@gmail.com>
2022-02-22 09:47:45 -0300

    gitignore and concatenation change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,21 @@
 fun main() {
     //Exemplo de conversa
-    var p1: String = "Dj Rogerio"
-    var p2: String = "Jose"
+    val p1: String = "Dj Rogerio"
+    val p2: String = "Jose"
 
-    println(p2 + ": I aew Jão, d boas?")
-    println(p1 + ": D boas man e tu?")
-    println(p2 + ": Então mano, tava afim d ouvir aquela braba ta ligado???")
-    println(p1 + ": Qual mano, não tenho bola de cristal!!!")
-    println(p2 + ": Pega leve chapa, fiko desumilde???")
-    println(p1 + ": Não mano, vc chega apavorando, como vou saber?")
-    println(p2 + ": Malz ae entao, tava afim de ouvir Cleiton Rasta pod pah?")
-    println(p1 + ": Gostei da ideia, acabando essa ja dou o play")
-    println(p2 + ": Então mano, pensei melhor, nao quero mais Cleiton Rasta!!!")
-    println(p1 + ": Po mano, como assim, ja ta de sacanagem com a minha face!!! Se decide Jão!!!")
-    println(p2 + ": Não me apressa to pensando.")
-    println(p1 + ": (DJ Rogerio sacou um facão, e meteu um apavoro) Coé, vai decidir NÃOOOOO!!! ")
-    println(p2 + ": Coé moh stresss, soh queria curtir a vibe mas desse jeito fiko de caozada!!!")
-    println(p1 + ": Sai andando então, arrumo nada!")
-    println(p2 + ": Já eh Rogerin nao queria ouvir mesmo, tó nem manda bem!!!!")
+    println("$p2: I aew Jão, d boas?")
+    println("$p1: D boas man e tu?")
+    println("$p2: Então mano, tava afim d ouvir aquela braba ta ligado???")
+    println("$p1: Qual mano, não tenho bola de cristal!!!")
+    println("$p2: Pega leve chapa, fiko desumilde???")
+    println("$p1: Não mano, vc chega apavorando, como vou saber?")
+    println("$p2: Malz ae entao, tava afim de ouvir Cleiton Rasta pod pah?")
+    println("$p1: Gostei da ideia, acabando essa ja dou o play")
+    println("$p2: Então mano, pensei melhor, nao quero mais Cleiton Rasta!!!")
+    println("$p1: Po mano, como assim, ja ta de sacanagem com a minha face!!! Se decide Jão!!!")
+    println("$p2: Não me apressa to pensando.")
+    println("$p1: (DJ Rogerio sacou um facão, e meteu um apavoro) Coé, vai decidir NÃOOOOO!!! ")
+    println("$p2: Coé moh stresss, soh queria curtir a vibe mas desse jeito fiko de caozada!!!")
+    println("$p1: Sai andando então, arrumo nada!")
+    println("$p2: Já eh Rogerin nao queria ouvir mesmo, tó nem manda bem!!!!")
 }
